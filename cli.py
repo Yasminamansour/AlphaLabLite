4842762d0ac37458523c10cfb7222dead77e9527
yasmina mansour <mansouryasmina2005@gmail.com>
2026-04-24 11:10:30 +0300

    Added -h and —h support
    Forgot to do that in my original implementation

--- cli.py
+++ cli.py
@@ -3,7 +3,8 @@ from transformation_impl import rules
 from services import execute, view
 from helper import read_commands, print_series
 arg = sys.argv
-if len(arg) < 2: #if the user did not enter any argument print the rules
+the user entered -h or --h 
+if len(arg) < 2 or arg[1] == "-h" or arg[1] == "--help": #if the user did not enter any argument print the rules
     print(rules())
 elif arg[1] == "execute": # if he enteted execute
     commands = read_commands() #prompyt him for the commands he woul like to excute 
